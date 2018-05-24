b7e33ebbcd71c77d2b791a581b27129f116a906d
Jean-christophe Gay <jean-christophe.gay@dauphine.fr>
2018-05-24 15:04:02 +0200

    first example computational graph => example.py

---
+++ example.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+
+# coding: utf-8
+
+import numpy
+
+class Variable:
+    """Represents a variable (i.e. an intrinsic, changeable parameter of a computational graph)."""
+    def __init__(self, initial_value=None):
+        """Construct Variable
+
+        Args:
+          initial_value: The initial value of this variable
+        """
+        self.value = initial_value
+        self.consumers = []
+        # Append this variable to the list of variables in the currently active default graph
+        global _default_graph
+        _default_graph.variables.append(self)
+
+class Graph:
+    """Represents a computational graph"""
+    def __init__(self):
+        """Construct Graph"""
+        self.operations = []
+        self.placeholders = []
+        self.variables = []
+    def as_default(self):
+        global _default_graph
+        _default_graph = self
+
+class Placeholder:
+    """Represents a placeholder node that has to be provided with a value
+       when computing the output of a computational graph
+    """
+
+    def __init__(self):
+        """Construct placeholder"""
+        self.consumers = []
+
+        # Append this placeholder to the list of placeholders in the currently active default graph
+        _default_graph.placeholders.append(self)
+
+class Operation:
+    """Represents a graph node that performs a computation.
+
+    An `Operation` is a node in a `Graph` that takes zero or
+    more objects as input, and produces zero or more objects
+    as output.
+    """
+
+    def __init__(self, input_nodes=[]):
+        """Construct Operation"""
+        self.input_nodes = input_nodes
+
+        # Initialize list of consumers (i.e. nodes that receive this operation's output as input)
+        self.consumers = []
+
+        # Append this operation to the list of consumers of all input nodes
+        for input_node in input_nodes:
+            input_node.consumers.append(self)
+
+        # Append this operation to the list of operations in the currently active default graph
+        _default_graph.operations.append(self)
+
+    def compute(self):
+        """Computes the output of this operation.
+        "" Must be implemented by the particular operation.
+        """
+        pass
+
+class Matmul(Operation):
+    """Multiplies matrix a by matrix b, producing a * b.
+    """
+
+    def __init__(self, a, b):
+        """Construct matmul
+
+        Args:
+          a: First matrix
+          b: Second matrix
+        """
+        super().__init__([a, b])
+
+    def compute(self, a_value, b_value):
+        """Compute the output of the matmul operation
+
+        Args:
+          a_value: First matrix value
+          b_value: Second matrix value
+        """
+        return a_value.dot(b_value)
+
+class Add(Operation):
+    """Returns x + y element-wise.
+    """
+
+    def __init__(self, x, y):
+        """Construct add
+
+        Args:
+          x: First summand node
+          y: Second summand node
+        """
+        super().__init__([x, y])
+
+    def compute(self, x_value, y_value):
+        """Compute the output of the add operation
+
+        Args:
+          x_value: First summand value
+          y_value: Second summand value
+        """
+        return x_value + y_value
+
+
+class Session:
+    """Represents a particular execution of a computational graph.
+    """
+
+    def run(self, operation, feed_dict={}):
+        """Computes the output of an operation
+
+        Args:
+          operation: The operation whose output we'd like to compute.
+          feed_dict: A dictionary that maps placeholders to values for this session
+        """
+
+        # Perform a post-order traversal of the graph to bring the nodes into the right order
+        nodes_postorder = traverse_postorder(operation)
+
+        # Iterate all nodes to determine their value
+        for node in nodes_postorder:
+
+            if type(node) == Placeholder:
+                # Set the node value to the placeholder value from feed_dict
+                node.output = feed_dict[node]
+            elif type(node) == Variable:
+                # Set the node value to the variable's value attribute
+                node.output = node.value
+            else:  # Operation
+                # Get the input values for this operation from the output values of the input nodes
+                node.inputs = [input_node.output for input_node in node.input_nodes]
+
+                # Compute the output of this operation
+                node.output = node.compute(*node.inputs)
+
+            # Convert lists to numpy arrays
+            if type(node.output) == list:
+                node.output = numpy.array(node.output)
+
+        # Return the requested node value
+        return operation.output
+
+
+def traverse_postorder(operation):
+    """Performs a post-order traversal, returning a list of nodes
+    in the order in which they have to be computed
+
+    Args:
+       operation: The operation to start traversal at
+    """
+
+    nodes_postorder = []
+
+    def recurse(node):
+        if isinstance(node, Operation):
+            for input_node in node.input_nodes:
+                recurse(input_node)
+        nodes_postorder.append(node)
+
+    recurse(operation)
+    return nodes_postorder
+
+## Calculate z = [[1,0],[0,-1]] * x + [1,1]
+
+# Create new Graph
+Graph().as_default()
+
+a = Variable( [ [1,0], [0,-1] ] )
+b = Variable([1,1])
+
+# Create placeholders
+x = Placeholder()
+
+# Create hidden node y :
+y = Matmul(a,x)
+
+# Create output node z
+z = Add(y,b)
+
+session = Session()
+output = session.run(z, { x: [1,3]})
+print(output)
